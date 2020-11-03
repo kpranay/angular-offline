4e07d028a141bf2a3562657120150a3852408dec
Pranay Kotha <pranay.kotha@keste.com>
2020-11-03 21:29:17 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  moviesData = [];
+  
+  ngOnInit() {
+    const request = window.indexedDB.open('imdbmovies', 13);
+
+    request.onerror = (event: any) => {
+      // Do something with request.errorCode!
+      console.error('Database error: ' + event.target.errorCode);
+    };
+    request.onsuccess = (event: any) => {
+      // Do something with request.result!
+      const db = event.target.result;
+      const transaction = db.transaction(['movie']);
+      const objectStore = transaction.objectStore('movie');
+      const movieRequest = objectStore.getAll();
+      movieRequest.onerror = (event) => {
+        // Handle errors!
+      };
+      movieRequest.onsuccess = (event) => {
+        // Do something with the request.result!
+        console.log('Data is >> ', movieRequest.result.length);
+        this.moviesData = movieRequest.result.slice(0,100);
+      };
+    };
+  }
 }
